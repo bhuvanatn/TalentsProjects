9cd02a4b5feee7cfa7f64e185bea0b93042ae6d5
Bhuvana <bhuvanatn@gmail.com>
2019-05-13 16:42:37 +1000

    created web api, react component

--- Talents/ClientApp/src/App.js
+++ Talents/ClientApp/src/App.js
@@ -4,16 +4,21 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { FetchProducts } from './components/Product/FetchProducts';
+import { Container } from 'semantic-ui-react';
 
 export default class App extends Component {
   displayName = App.name
 
   render() {
     return (
-      <Layout>
+        <Layout>
+            <Container>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
+            <Route path='/fetchdata' component={FetchData} />
+                <Route path='/fetchproducts' component={FetchProducts} />
+                </Container>
       </Layout>
     );
   }
---
+++ Talents/ClientApp/src/components/Product/FetchProducts.js
@@ -0,0 +1,85 @@
+﻿import React, { Component } from 'react';
+import { Icon, Label, Menu, Table, Button } from 'semantic-ui-react';
+import axios from 'axios';
+
+
+export class FetchProducts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ProductData: []
+        }
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    componentDidMount() {
+        axios.get("https://localhost:44396/api/products").then(response => {
+            console.log(response.data);
+            this.setState({
+                ProductData: response.data
+            });
+        });
+    }
+    handleDelete(id) {
+        fetch('https://localhost:44396/api/product/' + id, {
+            method: 'delete'
+        }).then(data => {
+            this.setState(
+                {
+                    ProductData: this.state.ProductData.filter((rec) => {
+                        return (rec.id != id);
+                    })
+                });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2> Products List </h2>
+                <p>
+                    <a href="/addproduct"> Create New </a>
+                </p>
+
+                <div>
+                    <Button icon labelPosition='left'>
+                        <Icon name='pause' />
+                        Pause
+    </Button>
+                    <Button icon labelPosition='right'>
+                        Next
+      <Icon name='right arrow' />
+                    </Button>
+                </div>
+
+                <Button color='red'>Red</Button>
+                <Table celled>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Id</Table.HeaderCell>
+                            <Table.HeaderCell>Name</Table.HeaderCell>
+                            <Table.HeaderCell>Product</Table.HeaderCell>
+                            <Table.HeaderCell>Buttons</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+                        {this.state.ProductData.map((p) => {
+                            return (
+                                <Table.Row>
+                                    <Table.Cell>{p.id} </Table.Cell>
+                                    <Table.Cell>{p.productName}</Table.Cell>
+                                    <Table.Cell>{p.price}</Table.Cell>
+                                    <Button color='red'>Red</Button>
+                                    <Table.Cell><a onClick={(id) => this.handleDelete(p.id)}>Delete</a> </Table.Cell>
+                                </Table.Row>
+                            );
+                        })}
+                    </Table.Body>
+
+
+                </Table>
+            </div>
+        );
+    }
+}
